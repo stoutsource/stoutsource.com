3d1a7a65a81d8ebccea190bb8c2ed7345ba31820
YuziRussianHax <yuzirock@gmail.com>
2017-02-08 18:35:40 +0200

    Base project configuration

---
+++ README.md
@@ -0,0 +1,5 @@
+# Run project
+To run project just type this in console in root of project
+```
+npm i && npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// Static Server + watching sass/js/html files
+gulp.task('watch', ['sass', 'html'], function () {
+  browserSync.init({
+    server: "./dist"
+  });
+
+  gulp.watch("src/sass/**/*.sass", ['sass']);
+  gulp.watch("src/js/**/*.js", ['js']);
+  gulp.watch("src/*.html", ['html'])
+    .on('change', browserSync.reload);
+});
+
+// Compile Sass into CSS
+gulp.task('sass', function () {
+  return gulp.src("src/sass/**/*.sass")
+    .pipe(sass())
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+});
+
+// Copy html to dist directory
+gulp.task('html', function () {
+  return gulp.src("src/*.html")
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
