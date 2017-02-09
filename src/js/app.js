f7fc0d01a06d6be4858b003e585187dc1c8d581d
YuziRussianHax <yuzirock@gmail.com>
2017-02-09 16:01:30 +0200

    Add base header layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,10 @@
 // StoutSource
 console.log('%cNu zdorovo!', 'font-size: 32pt');
+
+var $root = $('html, body');
+$('a').click(function () {
+  $root.animate({
+    scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+  return false;
+});
