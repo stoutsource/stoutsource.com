d626d8b3518410571051fe10d8b49c9d3bb8b93f
YuziRussianHax <yuzirock@gmail.com>
2017-02-10 15:41:43 +0200

    Add mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,24 @@ console.log('%cNu zdorovo!', 'font-size: 32pt');
 
 var $root = $('html, body');
 
-$('a').click(function () {
+$('.header__link').click(function () {
+  $root.animate({
+    scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+
+  $('.header__hamburger').removeClass('header__hamburger--active');
+  $('.header__menu').removeClass('header__menu--mobile');
+  return false;
+});
+
+$('.welcome__scroll').click(function () {
   $root.animate({
     scrollTop: $( $.attr(this, 'href') ).offset().top
   }, 500);
   return false;
+});
+
+$('.header__hamburger').click(function () {
+  $(this).toggleClass('header__hamburger--active');
+  $('.header__menu').toggleClass('header__menu--mobile');
 });
